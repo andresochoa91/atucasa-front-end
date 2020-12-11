83bac88bfeb877070979572626372e7428778294
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-11 00:46:11 -0800

    EditLink set up, it is time to start setting deletion of products and links

--- src/components/Links/EditLink.tsx
+++ src/components/Links/EditLink.tsx
@@ -1,11 +1,66 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 const EditLink: FC<THandleMode & TLinksProps & TLinkProps> = ({ handleMode, handleLinks, link }): JSX.Element => {
+
+  const [ siteName, setSiteName ] = useState<string>(link.site_name);
+  const [ url, setUrl ] = useState<string>(link.url);
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    ( name === "siteName" ? setSiteName : setUrl)(value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    fetch(`http://localhost:3000/current_user/links/${link.id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        site_name: siteName,
+        url
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (!data.error) {
+        console.log(data);
+        handleLinks();
+        handleMode();
+      } else {
+        console.log(data);
+      }
+    })
+    .catch(console.error);
+  };
+
   return (
     <>
-      <h1>In Edit Mode</h1>
-      <button onClick={ handleMode }>Cancel</button>
-    </> 
+      <h2>Edit Link</h2>
+      <form onSubmit={ handleSubmit }>
+        <label>Site Name</label>   
+        <input 
+          type="text"
+          name="siteName"
+          value={ siteName }  
+          onChange={ handleInput } 
+        />
+        <br/>
+        <label>Url</label>   
+        <input 
+          type="text"
+          name="url"
+          value={ url }  
+          onChange={ handleInput } 
+        />
+        <br/>
+        <input type="submit" value="Submit"/>
+        <button onClick={ handleMode }>Cancel</button>
+      </form> 
+    </>
   );
 };
 
