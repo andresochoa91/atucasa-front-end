52a4b16c8d1db55e8d1d66032dde5a89adf948c0
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-16 16:00:26 -0800

    Ready to start orders component

--- undefined/home/andres/Desktop/codethedream/finalproject/atucasa-front-end/src/components/ShowProducts/ShowProducts.jsx
+++ undefined/home/andres/Desktop/codethedream/finalproject/atucasa-front-end/src/components/ShowProducts/ShowProducts.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ShowProduct from '../ShowProducts/ShowProduct';
 ;
 ;
 var ShowProducts = function (_a) {
     var products = _a.products, merchantID = _a.merchantID, currentCustomerID = _a.currentCustomerID;
     var _b = useState([]), cart = _b[0], setCart = _b[1];
-    var total = function () {
-        return Number(cart.reduce(function (acc, pr) {
-            return acc + ((pr.tax + pr.unitPrice) * pr.amount);
-        }, 0).toFixed(2));
+    var _c = useState(""), tip = _c[0], setTip = _c[1];
+    var _d = useState(0), total = _d[0], setTotal = _d[1];
+    var handleTip = function (event) {
+        setTip(event.target.value);
     };
+    var handleSuggestedTip = function () {
+        setTip((total * 0.15).toFixed(2));
+    };
+    useEffect(function () {
+        setTotal(cart.reduce(function (acc, pr) {
+            return acc + ((pr.tax + pr.unitPrice) * pr.amount);
+        }, 0));
+    }, [cart, tip]);
     var handleCheckout = function () {
         var checkout = {
             accepted: true,
             current_user: "merchant",
+            tip: Number(tip),
             delivery_fee: 5,
             products: cart.map(function (pr) {
                 return {
@@ -38,6 +47,7 @@ var ShowProducts = function (_a) {
             .then(function (data) {
             console.log(data);
             setCart([]);
+            setTip("");
         })
             .catch(console.error);
     };
@@ -60,17 +70,34 @@ var ShowProducts = function (_a) {
         <tbody>
           {cart.map(function (cartProduct, cID) { return (<tr key={cID}>
                 <td>{cartProduct.productName}</td>
-                <td>{cartProduct.unitPrice}</td>
+                <td>${cartProduct.unitPrice}</td>
                 <td>{cartProduct.amount}</td>
-                <td>{cartProduct.tax}</td>
-                <td>{((cartProduct.tax + cartProduct.unitPrice) * cartProduct.amount).toFixed(2)}</td>
+                <td>${cartProduct.tax.toFixed(2)}</td>
+                <td>${((cartProduct.tax + cartProduct.unitPrice) * cartProduct.amount).toFixed(2)}</td>
               </tr>); })}
           <tr>
             <td></td>
             <td></td>
             <td></td>
+            <td><strong>Tip</strong></td>
+            <td>
+              $<input type="text" value={tip} placeholder={"Suggested: " + (total * 0.15).toFixed(2)} onChange={handleTip}/>
+            </td>
+            <td><button onClick={handleSuggestedTip}>Apply suggested tip</button></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>Delivery Fee</strong></td>
+            <td>$5</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
             <td><strong>Total</strong></td>
-            <td><strong>{total()}</strong></td>
+            <td><strong>${(total + Number(tip) + 5).toFixed(2)}</strong></td>
           </tr>
         </tbody>
       </table>
