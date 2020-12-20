9585465c69f2349082361cdb27c07b4ab4fd2a5b
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-19 23:45:23 -0800

    Order component created Orders component refactored

---
+++ src/components/Orders/Order.tsx
@@ -0,0 +1,106 @@
+import React, { FC, useContext } from 'react';
+import { AtucasaContext } from '../../Context';
+
+interface IOrderProps {
+  order: TOrder
+}
+
+const Order: FC<IOrderProps> = ({ order }): JSX.Element => {
+
+  const { currentUser } = useContext<TContextProps>(AtucasaContext)
+
+
+  const handleRole = (role:string, id:number): void => {
+    fetch(`${process.env.REACT_APP_API}/current_user/orders/${id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        current_user: role === "merchant" ? "customer" : "merchant"
+      })
+    })
+  };
+
+  return (
+    <div key={ order.id }>
+      <h3>Order #{ order.id }</h3>
+      <table style={{ textAlign: "center" }}>
+        <thead>
+          <tr>
+            <th>Product name</th>
+            <th>Unit Price</th>
+            <th>Amount</th>
+            <th>Unit Tax</th>
+            <th>Semi Total</th>
+          </tr>
+        </thead>          
+        <tbody>
+          {
+            order.products_order.map(product => (                  
+              <tr key={ product.id }>
+                <td>{ product.product_name }</td>
+                <td>${ (product.price).toFixed(2) }</td>
+                <td>{ product.amount }</td>
+                <td>${ (product.tax).toFixed(2) }</td>
+                <td>${ Number(((product.price + product.tax) * product.amount).toFixed(2)) }</td>
+              </tr>               
+            ))
+          }
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>Tip</strong></td>
+            <td>${ order.tip }</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>Delivery Fee</strong></td>
+            <td>${ order.delivery_fee }</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>Total</strong></td>
+            <td>
+              ${ 
+                (order.products_order.reduce((acc, pr) => {
+                  return (acc + ((pr.price + pr.tax) * pr.amount));
+                }, 0) + order.tip + order.delivery_fee).toFixed(2)
+              }
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {
+        currentUser?.role === "customer" && order.current_user === "customer" && !order.accepted ? (
+          <>
+            <p>If you accept the changes, press confirm order, if not, press Cancel Order</p>
+            <button>Confirm order</button>
+            <button>Calcel order</button>
+          </>
+        ) : currentUser?.role === "customer" && order.current_user === "merchant" && !order.accepted ? (
+          <p>Waiting for the merchant to confirm your order.</p>
+        ) : currentUser?.role === "merchant" && order.current_user === "merchant" && !order.accepted ? (
+          <>
+            <p>If you have all the products, press confirm order, if not, press Suggest Changes.</p>
+            <button>Confirm order</button>
+            <button onClick={ () => handleRole(order.current_user, order.id) } >Suggest changes</button>
+          </>
+        ) : currentUser?.role === "merchant" && order.current_user === "customer" && !order.accepted ? (
+          <p>Waiting for the customer to confirm your order.</p>
+        ) : (
+          <p style={{color: "#0a0"}}>Order accepted</p>
+        )
+      }
+      <br/>
+    </div>
+  );
+};
+
+export default Order;
---
+++ src/components/Orders/dist/Order.js
@@ -0,0 +1,83 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var Context_1 = require("../../Context");
+var Order = function (_a) {
+    var order = _a.order;
+    var currentUser = react_1.useContext(Context_1.AtucasaContext).currentUser;
+    var handleRole = function (role, id) {
+        fetch(process.env.REACT_APP_API + "/current_user/orders/" + id, {
+            method: "PUT",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                current_user: role === "merchant" ? "customer" : "merchant"
+            })
+        });
+    };
+    return (react_1["default"].createElement("div", { key: order.id },
+        react_1["default"].createElement("h3", null,
+            "Order #",
+            order.id),
+        react_1["default"].createElement("table", { style: { textAlign: "center" } },
+            react_1["default"].createElement("thead", null,
+                react_1["default"].createElement("tr", null,
+                    react_1["default"].createElement("th", null, "Product name"),
+                    react_1["default"].createElement("th", null, "Unit Price"),
+                    react_1["default"].createElement("th", null, "Amount"),
+                    react_1["default"].createElement("th", null, "Unit Tax"),
+                    react_1["default"].createElement("th", null, "Semi Total"))),
+            react_1["default"].createElement("tbody", null,
+                order.products_order.map(function (product) { return (react_1["default"].createElement("tr", { key: product.id },
+                    react_1["default"].createElement("td", null, product.product_name),
+                    react_1["default"].createElement("td", null,
+                        "$",
+                        (product.price).toFixed(2)),
+                    react_1["default"].createElement("td", null, product.amount),
+                    react_1["default"].createElement("td", null,
+                        "$",
+                        (product.tax).toFixed(2)),
+                    react_1["default"].createElement("td", null,
+                        "$",
+                        Number(((product.price + product.tax) * product.amount).toFixed(2))))); }),
+                react_1["default"].createElement("tr", null,
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null,
+                        react_1["default"].createElement("strong", null, "Tip")),
+                    react_1["default"].createElement("td", null,
+                        "$",
+                        order.tip)),
+                react_1["default"].createElement("tr", null,
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null,
+                        react_1["default"].createElement("strong", null, "Delivery Fee")),
+                    react_1["default"].createElement("td", null,
+                        "$",
+                        order.delivery_fee)),
+                react_1["default"].createElement("tr", null,
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null),
+                    react_1["default"].createElement("td", null,
+                        react_1["default"].createElement("strong", null, "Total")),
+                    react_1["default"].createElement("td", null,
+                        "$",
+                        (order.products_order.reduce(function (acc, pr) {
+                            return (acc + ((pr.price + pr.tax) * pr.amount));
+                        }, 0) + order.tip + order.delivery_fee).toFixed(2))))),
+        (currentUser === null || currentUser === void 0 ? void 0 : currentUser.role) === "customer" && order.current_user === "customer" && !order.accepted ? (react_1["default"].createElement(react_1["default"].Fragment, null,
+            react_1["default"].createElement("p", null, "If you accept the changes, press confirm order, if not, press Cancel Order"),
+            react_1["default"].createElement("button", null, "Confirm order"),
+            react_1["default"].createElement("button", null, "Calcel order"))) : (currentUser === null || currentUser === void 0 ? void 0 : currentUser.role) === "customer" && order.current_user === "merchant" && !order.accepted ? (react_1["default"].createElement("p", null, "Waiting for the merchant to confirm your order.")) : (currentUser === null || currentUser === void 0 ? void 0 : currentUser.role) === "merchant" && order.current_user === "merchant" && !order.accepted ? (react_1["default"].createElement(react_1["default"].Fragment, null,
+            react_1["default"].createElement("p", null, "If you have all the products, press confirm order, if not, press Suggest Changes."),
+            react_1["default"].createElement("button", null, "Confirm order"),
+            react_1["default"].createElement("button", { onClick: function () { return handleRole(order.current_user, order.id); } }, "Suggest changes"))) : (currentUser === null || currentUser === void 0 ? void 0 : currentUser.role) === "merchant" && order.current_user === "customer" && !order.accepted ? (react_1["default"].createElement("p", null, "Waiting for the customer to confirm your order.")) : (react_1["default"].createElement("p", { style: { color: "#0a0" } }, "Order accepted")),
+        react_1["default"].createElement("br", null)));
+};
+exports["default"] = Order;
