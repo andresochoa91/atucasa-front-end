9c3b4d6dadace6cb55f2e7831f494b41e4209562
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-07 10:55:17 -0800

    Routes for login and logout set up

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,43 @@ import SignIn from './components/SignForms/SignUp';
 import SignOut from './components/SignForms/SignOut';
 import Customer from './components/Customer/Customer';
 import Merchant from './components/Merchant/Merchant';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 const App:FC = () => {
   const { currentUser } = useContext<TContextProps>(AtucasaContext);
   return (
     <div>
       <h1>Welcome to atucasa.com</h1>
-      {
-        currentUser ?
-        <>
-          <SignOut />
-          {
-            currentUser.role === "customer" ?
-              <Customer />
-            :
-              <Merchant />
-          }
-        </> :
-        <>
-          <SignUp />
-          <SignIn />
-        </>
-      }
+      <Switch>
+        {
+          currentUser ? (
+            <>
+              <Route exact path="/" render={() => <Redirect to="/home" />}/>
+              <Route path="/home" render={() => (
+                <>
+                  <SignOut />
+                  {
+                    currentUser.role === "customer" ?
+                      <Customer />
+                    :
+                      <Merchant />
+                  }
+                </> 
+              )}/>
+            </>
+          ) : (
+            <>
+              <Redirect to="/" />
+              <Route exact path="/" render={() => (
+                <>
+                  <SignUp />
+                  <SignIn />
+                </>
+              )}/>
+            </>
+          )
+        }
+      </Switch>
     </div>
   );
 };
