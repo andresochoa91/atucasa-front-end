6b306256bcba99d18bc3a856a656a59d672ce53d
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-08 15:07:31 -0800

    Coming back adding /home, adding new route for future merchants

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SignIn from './components/SignForms/SignUp';
 import SignOut from './components/SignForms/SignOut';
 import Customer from './components/Customer/Customer';
 import Merchant from './components/Merchant/Merchant';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
 
 const App:FC = () => {
   const { currentUser, loggedOut } = useContext<TContextProps>(AtucasaContext);
@@ -18,14 +18,16 @@ const App:FC = () => {
           (loggedOut || !currentUser) ? (
             <>
               {
-                  loggedOut && <Redirect to="/" />
+                loggedOut && <Redirect to="/" />
               }
               <Route exact path="/" render={() => (
                 <>
                   <SignUp />
                   <SignIn />
+                  <Link to="/whatever">Yayaya</Link>
                 </>
               )}/>
+              <Route path="/whatever" render={() => <h1>Yayayayaya</h1>} />
             </>
           ) : (
             <>
@@ -36,8 +38,10 @@ const App:FC = () => {
                   {
                     currentUser.role === "customer" ? <Customer /> : <Merchant />
                   }
+                  <Link to="/whatever">Yayaya</Link>
                 </> 
               )}/>
+              <Route exact path="/whatever" render={() => <h1>Yayayayaya</h1>} />
             </>
           )
         }
