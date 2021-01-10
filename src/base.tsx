f730df60bf5859a2c23f243b7863aaa956f73a34
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-10 00:02:41 -0800

    Keys updated, if something goes wrong come back to last commit

--- src/base.tsx
+++ src/base.tsx
@@ -2,12 +2,20 @@ import firebase from "firebase";
 import "firebase/storage";
 
 export const app = firebase.initializeApp({
-  "projectId": "leaflet-react",
-  "appId": "1:249401140596:web:177c70e0b412a67119b821",
-  "databaseURL": "https://leaflet-react-default-rtdb.firebaseio.com",
-  "storageBucket": "leaflet-react.appspot.com",
-  "locationId": "us-central",
-  "apiKey": "AIzaSyBJOu7zQTBJHahpqP_g7M6Xy8yORbaRblM",
-  "authDomain": "leaflet-react.firebaseapp.com",
-  "messagingSenderId": "249401140596"
+  // "projectId": "leaflet-react",
+  // "appId": "1:249401140596:web:177c70e0b412a67119b821",
+  // "databaseURL": "https://leaflet-react-default-rtdb.firebaseio.com",
+  // "storageBucket": "leaflet-react.appspot.com",
+  // "locationId": "us-central",
+  // "apiKey": "AIzaSyBJOu7zQTBJHahpqP_g7M6Xy8yORbaRblM",
+  // "authDomain": "leaflet-react.firebaseapp.com",
+  // "messagingSenderId": "249401140596"
+  "projectId": process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  "appId": process.env.REACT_APP_FIREBASE_APP_ID,
+  "databaseURL": process.env.REACT_APP_FIREBASE_DATABASE,
+  "storageBucket": process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  "locationId": process.env.REACT_APP_FIREBASE_LOCATION_ID,
+  "apiKey": process.env.REACT_APP_FIREBASE_KEY,
+  "authDomain": process.env.REACT_APP_FIREBASE_DOMAIN,
+  "messagingSenderId": process.env.REACT_APP_FIREBASE_SENDER_ID   
 });
