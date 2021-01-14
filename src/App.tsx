251291f6da7686aa3dc8366c5d3bac9d00d96a2a
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-14 01:19:56 -0800

    Memory leak in mymap fixed

--- src/App.tsx
+++ src/App.tsx
@@ -9,62 +9,59 @@ import { Route, Switch, Redirect, Link } from 'react-router-dom';
 import MyMap from './components/MyMap/MyMap';
 import ShowMerchant from './components/ShowMerchant/ShowMerchant';
 
+
 const App:FC = (): JSX.Element => {
   const { currentUser, loggedOut } = useContext<TContextProps>(AtucasaContext);
 
   return (
     <div>
       <h1>Welcome to atucasa.com</h1>
-      {
-        (true) && (
-          <Switch>
-            {
-              (loggedOut || !currentUser) ? (
+      <Switch>
+        {
+          currentUser ? (
+            <>
+              <Route exact path="/" render={() => <Redirect to="/home" />}/>
+              <Route path="/home" render={() => (
                 <>
+                  <SignOut />
                   {
-                    loggedOut && <Redirect to="/" />
+                    currentUser.role === "customer" ? <Customer /> : <Merchant />
                   }
-                  <Route exact path="/" render={() => (
-                    <>
-                      <SignUp />
-                      <SignIn />
-                      <Link to="/home/map">Show Map</Link>
-                    </>
-                  )}/>
-                  {/* <Route path="/merchants" render={() => <ShowMerchants />} /> */}
-                  <Route path="/home/map" render={() => <MyMap />} />
-                  <Route 
-                    exact path="/merchants/:slug" 
-                    render={(props) => (
-                      <ShowMerchant {...props} />
-                    )} 
-                  />
-                </>
-              ) : (
+                  {/* <Link to="/merchants">Yayaya</Link> */}
+                </> 
+              )}/>
+              {/* <Route exact path="/merchants" render={() => <ShowMerchants />} /> */}
+              <Route 
+                exact path="/merchants/:slug" 
+                render={(props) => (
+                  <ShowMerchant {...props} />
+                )} 
+              />
+            </>
+          ) : (
+            <>
+              {
+                loggedOut && <Redirect to="/" />
+              }
+              <Route exact path="/" render={() => (
                 <>
-                  <Route exact path="/" render={() => <Redirect to="/home" />}/>
-                  <Route path="/home" render={() => (
-                    <>
-                      <SignOut />
-                      {
-                        currentUser.role === "customer" ? <Customer /> : <Merchant />
-                      }
-                      {/* <Link to="/merchants">Yayaya</Link> */}
-                    </> 
-                  )}/>
-                  {/* <Route exact path="/merchants" render={() => <ShowMerchants />} /> */}
-                  <Route 
-                    exact path="/merchants/:slug" 
-                    render={(props) => (
-                      <ShowMerchant {...props} />
-                    )} 
-                  />
+                  <SignUp />
+                  <SignIn />
+                  <Link to="/map">Show Map</Link>
                 </>
-              )
-            }
-          </Switch>
-        )
-      }
+              )}/>
+              {/* <Route path="/merchants" render={() => <ShowMerchants />} /> */}
+              <Route path="/map" render={() => <MyMap />} />
+              <Route 
+                exact path="/merchants/:slug" 
+                render={(props) => (
+                  <ShowMerchant {...props} />
+                )} 
+              />
+            </>
+          )
+        }
+      </Switch>
     </div>
   );
 };
